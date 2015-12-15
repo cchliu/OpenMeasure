dfcb0adb1dc5d953fc61003169577ee163a5f42a
cchliu <cchliu@ucdavis.edu>
2015-12-15 15:56:02 -0800

    input/output file names declaration

---
+++ analyze/fileParams.py
@@ -0,0 +1,23 @@
+# network topology
+topo_file = '../Geant/Geant_graph.pkl'
+
+# network trace data
+#infile_lst = ['../Geant/scaleNewGeantTraces_0108.csv', '../Geant/scaleNewGeantTraces_0215.csv', '../Geant/scaleNewGeantTraces_0324.csv', '../Geant/scaleNewGeantTraces_0401.csv']
+
+infile_lst = ['../Geant/scaleNewGeantTraces_2week_0102.csv']
+
+#outfile_lst = ['../Geant/scaleNewGeantTraces_0108.csv.tmp', '../Geant/scaleNewGeantTraces_0215.csv.tmp', '../Geant/scaleNewGeantTraces_0324.csv.tmp', '../Geant/scaleNewGeantTraces_0401.csv.tmp']
+
+outfile_lst = ['../Geant/scaleNewGeantTraces_2week_0102.csv.tmp']
+
+# flow route file
+flow_route_file = '../Geant/Geant_flow_route.json'
+output_flow_route_file = '../Geant/2week_Geant_flow_route.json.tmp'
+
+# sw route file
+sw_route_file = '../Geant/Geant_sw_route.json'
+output_sw_route_file = '../Geant/2week_Geant_sw_route.json.tmp'
+
+# prefix file
+prefix_outfile = '../Geant/2week_prefix.csv.tmp'
+#prefix_outfile = '../Geant/prefix.csv.tmp'
