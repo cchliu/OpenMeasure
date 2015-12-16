b040637f4a99c9540e37fec82c66bd0b843043ef
cchliu <cchliu@ucdavis.edu>
2015-12-16 12:49:09 -0800

    input file parameters

---
+++ simulator/fileParams.py
@@ -0,0 +1,12 @@
+#infile_lst = ['../../input/Geant/scaleNewGeantTraces_0108.csv.tmp', '../../input/Geant/scaleNewGeantTraces_0215.csv', '../../input/Geant/scaleNewGeantTraces_0324.csv', '../../input/Geant/scaleNewGeantTraces_0401.csv']
+
+infile_lst = ['../../input/Geant/scaleNewGeantTraces_2week_0102.csv.tmp']
+
+#sw_route_file = '../../input/Geant/Geant/Geant_sw_route.json.tmp'
+sw_route_file = '../../input/Geant/2week_Geant_sw_route.json.tmp'
+
+#flow_route_file = '../../input/Geant/Geant_flow_route.json.tmp'
+flow_route_file = '../../input/Geant/2week_Geant_flow_route.json.tmp'
+
+#prefix_file = '../../input/Geant/prefix.csv.tmp'
+prefix_file = '../../input/Geant/2week_prefix.csv.tmp'
