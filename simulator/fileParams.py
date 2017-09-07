e5e7c06ed0de01177b8c45900408be043884ed11
cchliu <cchliu@ucdavis.edu>
2017-09-07 10:30:31 -0700

    update fileParams.py

--- simulator/fileParams.py
+++ simulator/fileParams.py
@@ -2,7 +2,7 @@
 
 infile_lst = ['../../input/Geant/scaleNewGeantTraces_2week_0102.csv.tmp']
 
-#sw_route_file = '../../input/Geant/Geant/Geant_sw_route.json.tmp'
+#sw_route_file = '../../input/Geant/Geant_sw_route.json.tmp'
 sw_route_file = '../../input/Geant/2week_Geant_sw_route.json.tmp'
 
 #flow_route_file = '../../input/Geant/Geant_flow_route.json.tmp'
